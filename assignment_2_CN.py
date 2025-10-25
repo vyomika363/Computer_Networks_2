f709a2607625d47edfc5b0283e40ca507f931df1
Vyomika <143392232+vyomika363@users.noreply.github.com>
2025-10-25 15:23:45 +0530

    Update assignment_2_CN.py

--- assignment_2_CN.py
+++ assignment_2_CN.py
@@ -7,44 +7,39 @@ from mininet.log import setLogLevel
 
 class CustomTopo(Topo):
     def build(self):
-        
-        H1 = self.addHost('H1', ip='10.0.0.1/24')
-        H2 = self.addHost('H2', ip='10.0.0.2/24')
-        H3 = self.addHost('H3', ip='10.0.0.3/24')
-        H4 = self.addHost('H4', ip='10.0.0.4/24')
-        DNS = self.addHost('DNS', ip='10.0.0.5/24')
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+        h3 = self.addHost('h3', ip='10.0.0.3/24')
+        h4 = self.addHost('h4', ip='10.0.0.4/24')
+        dns = self.addHost('dns', ip='10.0.0.5/24')
 
-        #adding switches
-        S1 = self.addSwitch('S1')
-        S2 = self.addSwitch('S2')
-        S3 = self.addSwitch('S3')
-        S4 = self.addSwitch('S4')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
 
-        #addinf host to switch links with bw=100Mbps and delay=2ms
-        self.addLink(H1, S1, bw=100, delay='2ms')
-        self.addLink(H2, S2, bw=100, delay='2ms')
-        self.addLink(H3, S3, bw=100, delay='2ms')
-        self.addLink(H4, S4, bw=100, delay='2ms')
+        self.addLink(h1, s1, cls=TCLink, bw=100, delay='2ms')
+        self.addLink(h2, s2, cls=TCLink, bw=100, delay='2ms')
+        self.addLink(h3, s3, cls=TCLink, bw=100, delay='2ms')
+        self.addLink(h4, s4, cls=TCLink, bw=100, delay='2ms')
 
-        #adding switch to switch links
-        self.addLink(S1, S2, bw=1000, delay='5ms')
-        self.addLink(S2, S3, bw=1000, delay='8ms')
-        self.addLink(S3, S4, bw=1000, delay='10ms')
+        self.addLink(s1, s2, cls=TCLink, bw=1000, delay='5ms')
+        self.addLink(s2, s3, cls=TCLink, bw=1000, delay='8ms')
+        self.addLink(s3, s4, cls=TCLink, bw=1000, delay='10ms')
 
-        #linking DNS resolver to s2 
-        self.addLink(DNS, S2, bw=1000, delay='1ms')
+        self.addLink(dns, s2, cls=TCLink, bw=1000, delay='1ms')
+
 
 def run():
     topo = CustomTopo()
-    net = Mininet(topo=topo, link=TCLink)
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
     net.start()
 
-    print("Testing network connectivity")
+    print("*** Testing network connectivity")
     net.pingAll()
 
-    #starting CLI for manual testing
+    print("*** Starting Mininet CLI")
     CLI(net)
-
     net.stop()
 
 if __name__ == '__main__':
